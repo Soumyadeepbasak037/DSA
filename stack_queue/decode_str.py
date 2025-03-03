86e493b42dc1d5fe48964c809480ac83e0c7a860
Soumyadeepbasak037 <soumyadeep.basak06@gmail.com>
2025-03-03 19:21:15 +0530

    stacks and queues

--- stack_queue/decode_str.py
+++ stack_queue/decode_str.py
@@ -1,13 +1,46 @@
-s = "3[a]2[bc]"
-stack = []
+s = "100[leetcode]"
+# stack = []
+# accum = []
+# for i in s:
+#     if (i!="]"):
+#         stack.append(i)
+#         # print(stack)
+#     else:
+#         res = []
+#         while stack and stack[-1] != '[':
+#             alpha = stack.pop()
+#             res.insert(0, alpha)
+#             print(res)
+#             accum.append(res)
+#     #     stack.pop()
+#     # num = stack.pop()
+# #     stack.append(num*alpha)
+# # print(stack)
 
-for i in s:
-    if (i.isnumeric or i == "["):
-        stack.append(i)
+# print(accum)
+
+
+stack = []
+for char in s:
+    if char != "]":
+        stack.append(char)
     else:
+        substr = ""
         while stack and stack[-1] != "[":
-            alpha = stack.pop()
+            substr = stack.pop()+substr
         stack.pop()
-    num = stack.pop()
-    stack.append(num*alpha)
-print(stack)
+
+        num = ""
+        flag = False
+        while stack and stack[-1].isdigit() or flag == True:
+            num += stack.pop()
+            print(num)
+            if (stack and stack[-1].isdigit):
+                flag == True
+
+        rev = ''.join(reversed(num))
+        # print(rev)
+        stack.append(int(rev) * substr)
+
+res = "".join(stack)
+print(res)
